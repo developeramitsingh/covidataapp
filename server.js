45b64bb0c1beace267a1e15a3081c227b14a42bb
amit <shriamits454@gmail.com>
2020-07-19 19:38:08 +0530

    final Files

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production'){
   app.use(express.static("client/build"));
 
   app.get('*', (req, res)=>{
-    res.send(path.resolve(__dirname, 'client', 'coviddataapp', 'build', 'index.html'));
+    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
